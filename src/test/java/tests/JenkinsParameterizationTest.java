e008e65d42ce26989260dd26c7980c523bc1f00d
TRACK QJSPIDERS <TRACK QJSPIDERS@DESKTOP-2L70A79>
2023-09-06 08:44:59 +0530

    @@

---
+++ src/test/java/tests/JenkinsParameterizationTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsParameterizationTest {
+
+	@Test
+	public void demo() throws InterruptedException {
+		WebDriver driver = null;
+		String browser = System.getProperty("browser");
+		switch(browser) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		default: 
+			System.out.println("Invalid browser info");
+		}
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
